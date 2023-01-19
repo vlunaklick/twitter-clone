b475b980f10b52d847788c1c094a1b1a90f235c6
Valentín Lunaklick <84046275+vlunaklick@users.noreply.github.com>
2023-01-19 20:24:11 -0300

    style button header and leftarrow

--- src/components/app/Button.js
+++ src/components/app/Button.js
@@ -1,8 +1,25 @@
-export default function Button ({ children, onClick }) {
+const VARIANTS = {
+  normal: 'border-slate-200 rounded text-slate-500 hover:bg-slate-100 px-3 border',
+  rounded: 'rounded-full border-black text-white px-4 bg-black border',
+  none: '',
+  disabled: 'opacity-30'
+}
+
+export default function Button ({ children, onClick, maxWidth = true, className, variant = 'normal', disabled = false }) {
   return (
     <button
-      className="p-2 w-full border border-slate-200 rounded text-slate-500 font-medium text-sm hover:bg-slate-100 transition-colors"
+      className={'py-2 w-full font-medium text-xs transition-colors' +
+        ' ' +
+        (maxWidth ? 'w-full' : 'w-fit') +
+        ' ' +
+        className +
+        ' ' +
+        VARIANTS[variant] +
+        ' ' +
+        (disabled ? VARIANTS.disabled : '')
+      }
       onClick={onClick}
+      disabled={disabled}
     >
       {children}
     </button>
--- src/components/app/Header.js
+++ src/components/app/Header.js
@@ -1,7 +1,7 @@
 export default function Header () {
   return (
-    <header className="sticky top-0 w-full p-3 border-b border-slate-200 h-[50px] backdrop-blur">
-      <h2 className="font-semibold">Inicio</h2>
+    <header className="sticky top-0 w-full border-b border-slate-200 h-[50px] backdrop-blur flex items-center">
+      <h2 className="font-semibold pl-3">Inicio</h2>
     </header>
   )
 }

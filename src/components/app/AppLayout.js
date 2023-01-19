4bb98d33593c1d53ffcf32bd53ab9133b958e8d3
Valentín Lunaklick <84046275+vlunaklick@users.noreply.github.com>
2023-01-19 00:19:20 -0300

    change name

--- src/components/app/AppLayout.js
+++ src/components/app/AppLayout.js
@@ -4,7 +4,7 @@ export default function AppLayout ({ children }) {
   return (
     <>
       <Head>
-        <title>Concers</title>
+        <title>Littera</title>
         <meta
           name="description"
           content="Start meeting people, start your community."
@@ -12,7 +12,7 @@ export default function AppLayout ({ children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-        <div className="h-screen min-[520px]:max-w-[414px] w-full shadow-lg bg-slate-50 mx-auto">
+        <div className="min-h-screen min-[520px]:max-w-[414px] w-full shadow-lg bg-slate-50 mx-auto">
           {children}
         </div>
     </>
--- src/components/pages/index/Hero.js
+++ src/components/pages/index/Hero.js
@@ -1,7 +1,7 @@
 export default function Hero () {
   return (
     <div className="bg-gradient-to-r from-[#0f091b] to-slate-900 flex flex-col gap-1 items-center pt-4 px-8 pb-9 rounded-md mt-36 w-full text-white">
-      <h1 className="font-bold text-6xl">concers</h1>
+      <h1 className="font-bold text-6xl">Littera</h1>
       <p className="text-xs">Start meeting people, start your community.</p>
     </div>
   )

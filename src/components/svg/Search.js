7dc93a0d034447c80f9d426ebda5d3215a3893c2
Valentín Lunaklick <84046275+vlunaklick@users.noreply.github.com>
2023-01-21 01:14:56 -0300

    add svgs

---
+++ src/components/svg/Search.js
@@ -0,0 +1,13 @@
+function Search (props) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}>
+      <path
+        fillRule="evenodd"
+        d="M14.385 15.446a6.75 6.75 0 111.06-1.06l5.156 5.155a.75.75 0 11-1.06 1.06l-5.156-5.155zm-7.926-1.562a5.25 5.25 0 117.43-.005l-.005.005-.005.004a5.25 5.25 0 01-7.42-.004z"
+        clipRule="evenodd"
+      ></path>
+    </svg>
+  )
+}
+
+export default Search
